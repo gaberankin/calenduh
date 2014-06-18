c9d1e08d024d16a3234263312db8284141c3dba7
gabe rankin <gabe.rankin@gmail.com>
2014-06-18 16:02:19 -0400

    adding ability to attach to input fields, a few formatting options

--- coffee/jquery.calenduh.coffee
+++ coffee/jquery.calenduh.coffee
@@ -14,6 +14,28 @@ class Calenduh
 		dayTemplate: '<span class="day">{day}</span>'
 		prevDayTemplate: null
 		nextDayTemplate: null
+	@localization:
+		months:
+			'1': ['Jan', 'January']
+			'2': ['Feb', 'February']
+			'3': ['Mar', 'March']
+			'4': ['Apr', 'April']
+			'5': ['May', 'May']
+			'6': ['Jun', 'June']
+			'7': ['Jul', 'July']
+			'8': ['Aug', 'August']
+			'9': ['Sep', 'September']
+			'10': ['Oct', 'October']
+			'11': ['Nov', 'November']
+			'12': ['Dec', 'December']
+		weekdays:
+			'0': ['S', 'Sun', 'Sunday']
+			'1': ['M', 'Mon', 'Monday']
+			'2': ['T', 'Tue', 'Tuesday']
+			'3': ['W', 'Wed', 'Wednesday']
+			'4': ['T', 'Thu', 'Thursday']
+			'5': ['F', 'Fri', 'Friday']
+			'6': ['S', 'Sat', 'Satuday']
 	constructor: (options) ->
 		@opts = $.extend(true, @opts, options)
 		if @opts.date isnt null
@@ -43,7 +65,6 @@ class Calenduh
 		return (new Date(@opts.year, @opts.month, 1)).getMonth()
 	getYear: () ->
 		return (new Date(@opts.year, @opts.month, 1)).getFullYear()
-
 	render: () ->
 		# get beginning of month, so that we can figure out where (in the week) the previous month begins
 		now = new Date(@opts.year, @opts.month, 1, 0, 0, 0, 0)
@@ -57,7 +78,7 @@ class Calenduh
 			prevDayStart = prevDayEnd - prev.getDay()
 			weekday = 0
 			for d in [prevDayStart..prevDayEnd]
-				html += "<li class=\"day-item prev-month day-#{weekday}\" data-year=\"#{prevYear}\" data-month=\"#{prevMonth}\" data-day=\"#{d}\">"
+				html += "<li class=\"day-item prev-month day-#{weekday}#{if Calenduh.isToday(prevYear, prevMonth, d) then ' calenduh-today' else ''}\" data-year=\"#{prevYear}\" data-month=\"#{prevMonth}\" data-day=\"#{d}\">"
 				if @opts.renderPrevNext
 					html += @opts.prevDayTemplate.replace(/{day}/g, d)
 				html += "</li>"
@@ -70,14 +91,14 @@ class Calenduh
 		for d in [mStart..mEnd]
 			date = new Date(@opts.year, @opts.month, d, 0, 0, 0)
 			weekday = date.getDay()
-			html += "<li class=\"day-item current-month day-#{weekday}\" data-year=\"#{curYear}\" data-month=\"#{curMonth}\" data-day=\"#{d}\">#{@opts.dayTemplate.replace(/{day}/g, d)}</li>"
+			html += "<li class=\"day-item current-month day-#{weekday}#{if Calenduh.isToday(curYear, curMonth, d) then ' calenduh-today' else ''}\" data-year=\"#{curYear}\" data-month=\"#{curMonth}\" data-day=\"#{d}\">#{@opts.dayTemplate.replace(/{day}/g, d)}</li>"
 		if weekday isnt 6
 			nextMonth = next.getMonth();
 			nextYear = next.getFullYear()
 			nextDayStart = 1
 			nextDayEnd = 6 - weekday
 			for d in [nextDayStart..nextDayEnd]
-				html += "<li class=\"day-item next-month day-#{weekday}\" data-year=\"#{nextYear}\" data-month=\"#{nextMonth}\" data-day=\"#{d}\">"
+				html += "<li class=\"day-item next-month day-#{weekday}#{if Calenduh.isToday(nextYear, nextMonth, d) then ' calenduh-today' else ''}\" data-year=\"#{nextYear}\" data-month=\"#{nextMonth}\" data-day=\"#{d}\">"
 				if @opts.renderPrevNext
 					html += @opts.nextDayTemplate.replace(/{day}/g, d)
 				html += "</li>"
@@ -88,17 +109,39 @@ class Calenduh
 			return html
 		@opts.target.html(html)
 		return
+	@isToday: (date, month = false, day = false) ->
+		if month isnt false and day isnt false
+			date = new Date(date, month, day)
+		return date.getMonth() is Today.getMonth() and date.getFullYear() is Today.getFullYear() and date.getDate() is Today.getDate()
+	@format: (format, date = null) ->
+		if date is null
+			date = format
+			format = 'mm/dd/yyyy'
+		d = date.getDate()
+		m = date.getMonth() + 1
+		yyyy = date.getFullYear()
+
+		dd = if d < 10 then '0' + d else d
+		mm = if m < 10 then '0' + m else m
+		mmm = Calenduh.localization.months[m + ''][0]
+		mmmm = Calenduh.localization.months[m + ''][1]
+		yy = (yyyy + '').substr(2)
+	
+		return format.replace(/yyyy/g, yyyy).replace(/yy/g, yy).replace(/mmmm/g, mmmm).replace(/mmm/g, mmm).replace(/mm/g, mm).replace(/m/g, m).replace(/dd/g, dd).replace(/d/g, d)
+
 (($) ->
 	$.fn.calenduh = (options) ->
 		return @each(() ->
 			$me = $(this);
 			me = @
-			$container = $('<div class="calenduh-container"></div>').appendTo($me);
+			@isInput = $me.is(":text")
+			$container = $('<div class="calenduh-container"></div>').appendTo('body').hide()
 			$controls = $("""<div class="calenduh-controls calenduh-cf">
 					<a class="prev-month">prev</a>
+					<span class="current-loc"></span>
 					<a class="next-month">next</a>
 				</div>""").appendTo($container);
-			$calendar = $('<div class="calenduh-calendar"></div>').appendTo($container);
+			$calendar = $('<div class="calenduh-calendar"></div>').appendTo($container)
 
 			options.target = $calendar
 			@c = new Calenduh(options)
@@ -138,17 +181,32 @@ class Calenduh
 					args[1] = new Date(y, m, d)
 				$selected.removeClass('current-day')
 				$d.addClass('current-day')
+				if me.isInput
+					val = Calenduh.format(args[0])
+					$me.val(val)
 				$me.trigger('calenduh.selectday', args)
+			)
+			$me.focus(() ->
+				pos = $me.position()
+				h = $me.outerHeight()
+				$container.css({
+					position: 'absolute'
+					top: pos.top + h
+					left: pos.left
+				}).show()
+				return
 			)
 			$controls.on('click', '.next-month', () ->
 				d = me.c.getDate()
 				me.c.setDate(new Date(d.getFullYear(), d.getMonth() + 1, 1))
+				$('.current-loc', $controls).text(Calenduh.format('mmm yyyy', me.c.getDate()))
 				if selectedDay isnt null
 					$("li[data-year=#{selectedDay.getFullYear()}][data-month=#{selectedDay.getMonth()}][data-day=#{selectedDay.getDate()}]", $calendar).addClass('current-day')
 				$me.trigger('calenduh.nextmonth', [me.c.getDate()])
 			).on('click', '.prev-month', () ->
 				d = me.c.getDate()
 				me.c.setDate(new Date(d.getFullYear(), d.getMonth() - 1, 1))
+				$('.current-loc', $controls).text(Calenduh.format('mmm yyyy', me.c.getDate()))
 				if selectedDay isnt null
 					$("li[data-year=#{selectedDay.getFullYear()}][data-month=#{selectedDay.getMonth()}][data-day=#{selectedDay.getDate()}]", $calendar).addClass('current-day')
 				$me.trigger('calenduh.prevmonth', [me.c.getDate()])
--- js/jquery.calenduh.js
+++ js/jquery.calenduh.js
@@ -19,6 +19,32 @@
       nextDayTemplate: null
     };
 
+    Calenduh.localization = {
+      months: {
+        '1': ['Jan', 'January'],
+        '2': ['Feb', 'February'],
+        '3': ['Mar', 'March'],
+        '4': ['Apr', 'April'],
+        '5': ['May', 'May'],
+        '6': ['Jun', 'June'],
+        '7': ['Jul', 'July'],
+        '8': ['Aug', 'August'],
+        '9': ['Sep', 'September'],
+        '10': ['Oct', 'October'],
+        '11': ['Nov', 'November'],
+        '12': ['Dec', 'December']
+      },
+      weekdays: {
+        '0': ['S', 'Sun', 'Sunday'],
+        '1': ['M', 'Mon', 'Monday'],
+        '2': ['T', 'Tue', 'Tuesday'],
+        '3': ['W', 'Wed', 'Wednesday'],
+        '4': ['T', 'Thu', 'Thursday'],
+        '5': ['F', 'Fri', 'Friday'],
+        '6': ['S', 'Sat', 'Satuday']
+      }
+    };
+
     function Calenduh(options) {
       this.opts = $.extend(true, this.opts, options);
       if (this.opts.date !== null) {
@@ -76,7 +102,7 @@
         prevDayStart = prevDayEnd - prev.getDay();
         weekday = 0;
         for (d = _i = prevDayStart; prevDayStart <= prevDayEnd ? _i <= prevDayEnd : _i >= prevDayEnd; d = prevDayStart <= prevDayEnd ? ++_i : --_i) {
-          html += "<li class=\"day-item prev-month day-" + weekday + "\" data-year=\"" + prevYear + "\" data-month=\"" + prevMonth + "\" data-day=\"" + d + "\">";
+          html += "<li class=\"day-item prev-month day-" + weekday + (Calenduh.isToday(prevYear, prevMonth, d) ? ' calenduh-today' : '') + "\" data-year=\"" + prevYear + "\" data-month=\"" + prevMonth + "\" data-day=\"" + d + "\">";
           if (this.opts.renderPrevNext) {
             html += this.opts.prevDayTemplate.replace(/{day}/g, d);
           }
@@ -92,7 +118,7 @@
       for (d = _j = mStart; mStart <= mEnd ? _j <= mEnd : _j >= mEnd; d = mStart <= mEnd ? ++_j : --_j) {
         date = new Date(this.opts.year, this.opts.month, d, 0, 0, 0);
         weekday = date.getDay();
-        html += "<li class=\"day-item current-month day-" + weekday + "\" data-year=\"" + curYear + "\" data-month=\"" + curMonth + "\" data-day=\"" + d + "\">" + (this.opts.dayTemplate.replace(/{day}/g, d)) + "</li>";
+        html += "<li class=\"day-item current-month day-" + weekday + (Calenduh.isToday(curYear, curMonth, d) ? ' calenduh-today' : '') + "\" data-year=\"" + curYear + "\" data-month=\"" + curMonth + "\" data-day=\"" + d + "\">" + (this.opts.dayTemplate.replace(/{day}/g, d)) + "</li>";
       }
       if (weekday !== 6) {
         nextMonth = next.getMonth();
@@ -100,7 +126,7 @@
         nextDayStart = 1;
         nextDayEnd = 6 - weekday;
         for (d = _k = nextDayStart; nextDayStart <= nextDayEnd ? _k <= nextDayEnd : _k >= nextDayEnd; d = nextDayStart <= nextDayEnd ? ++_k : --_k) {
-          html += "<li class=\"day-item next-month day-" + weekday + "\" data-year=\"" + nextYear + "\" data-month=\"" + nextMonth + "\" data-day=\"" + d + "\">";
+          html += "<li class=\"day-item next-month day-" + weekday + (Calenduh.isToday(nextYear, nextMonth, d) ? ' calenduh-today' : '') + "\" data-year=\"" + nextYear + "\" data-month=\"" + nextMonth + "\" data-day=\"" + d + "\">";
           if (this.opts.renderPrevNext) {
             html += this.opts.nextDayTemplate.replace(/{day}/g, d);
           }
@@ -114,6 +140,39 @@
       this.opts.target.html(html);
     };
 
+    Calenduh.isToday = function(date, month, day) {
+      if (month == null) {
+        month = false;
+      }
+      if (day == null) {
+        day = false;
+      }
+      if (month !== false && day !== false) {
+        date = new Date(date, month, day);
+      }
+      return date.getMonth() === Today.getMonth() && date.getFullYear() === Today.getFullYear() && date.getDate() === Today.getDate();
+    };
+
+    Calenduh.format = function(format, date) {
+      var d, dd, m, mm, mmm, mmmm, yy, yyyy;
+      if (date == null) {
+        date = null;
+      }
+      if (date === null) {
+        date = format;
+        format = 'mm/dd/yyyy';
+      }
+      d = date.getDate();
+      m = date.getMonth() + 1;
+      yyyy = date.getFullYear();
+      dd = d < 10 ? '0' + d : d;
+      mm = m < 10 ? '0' + m : m;
+      mmm = Calenduh.localization.months[m + ''][0];
+      mmmm = Calenduh.localization.months[m + ''][1];
+      yy = (yyyy + '').substr(2);
+      return format.replace(/yyyy/g, yyyy).replace(/yy/g, yy).replace(/mmmm/g, mmmm).replace(/mmm/g, mmm).replace(/mm/g, mm).replace(/m/g, m).replace(/dd/g, dd).replace(/d/g, d);
+    };
+
     return Calenduh;
 
   })();
@@ -124,8 +183,9 @@
         var $calendar, $container, $controls, $me, me, selectedDay;
         $me = $(this);
         me = this;
-        $container = $('<div class="calenduh-container"></div>').appendTo($me);
-        $controls = $("<div class=\"calenduh-controls calenduh-cf\">\n	<a class=\"prev-month\">prev</a>\n	<a class=\"next-month\">next</a>\n</div>").appendTo($container);
+        this.isInput = $me.is(":text");
+        $container = $('<div class="calenduh-container"></div>').appendTo('body').hide();
+        $controls = $("<div class=\"calenduh-controls calenduh-cf\">\n	<a class=\"prev-month\">prev</a>\n	<span class=\"current-loc\"></span>\n	<a class=\"next-month\">next</a>\n</div>").appendTo($container);
         $calendar = $('<div class="calenduh-calendar"></div>').appendTo($container);
         options.target = $calendar;
         this.c = new Calenduh(options);
@@ -147,7 +207,7 @@
           d = $d.attr('data-day');
           $me.trigger('calenduh.mouseleaveday', [new Date(y, m, d)]);
         }).on('click', '.day-item', function() {
-          var $d, $selected, args, d, m, y;
+          var $d, $selected, args, d, m, val, y;
           $d = $(this);
           if ($d.hasClass('current-day')) {
             return;
@@ -166,12 +226,27 @@
           }
           $selected.removeClass('current-day');
           $d.addClass('current-day');
+          if (me.isInput) {
+            val = Calenduh.format(args[0]);
+            $me.val(val);
+          }
           return $me.trigger('calenduh.selectday', args);
+        });
+        $me.focus(function() {
+          var h, pos;
+          pos = $me.position();
+          h = $me.outerHeight();
+          $container.css({
+            position: 'absolute',
+            top: pos.top + h,
+            left: pos.left
+          }).show();
         });
         return $controls.on('click', '.next-month', function() {
           var d;
           d = me.c.getDate();
           me.c.setDate(new Date(d.getFullYear(), d.getMonth() + 1, 1));
+          $('.current-loc', $controls).text(Calenduh.format('mmm yyyy', me.c.getDate()));
           if (selectedDay !== null) {
             $("li[data-year=" + (selectedDay.getFullYear()) + "][data-month=" + (selectedDay.getMonth()) + "][data-day=" + (selectedDay.getDate()) + "]", $calendar).addClass('current-day');
           }
@@ -180,6 +255,7 @@
           var d;
           d = me.c.getDate();
           me.c.setDate(new Date(d.getFullYear(), d.getMonth() - 1, 1));
+          $('.current-loc', $controls).text(Calenduh.format('mmm yyyy', me.c.getDate()));
           if (selectedDay !== null) {
             $("li[data-year=" + (selectedDay.getFullYear()) + "][data-month=" + (selectedDay.getMonth()) + "][data-day=" + (selectedDay.getDate()) + "]", $calendar).addClass('current-day');
           }
